b32eb1c758696d1625c93fd5ad944ee252e32f8f
Vitalii <fsow@yandex.ru>
2021-05-06 12:21:44 +0300

    improve admin panel

--- url_shortener/shortener/admin.py
+++ url_shortener/shortener/admin.py
@@ -2,5 +2,26 @@ from django.contrib import admin
 
 from shortener.models import URL, Domain
 
-admin.site.register(Domain)
-admin.site.register(URL)
+
+class DomainAdmin(admin.ModelAdmin):
+    list_display = ("name", "blocked")
+
+
+class URLAdmin(admin.ModelAdmin):
+    list_display = (
+        "url_original",
+        "url_short",
+        "domain",
+        "blocked",
+        "domain_blocked",
+    )
+
+    def domain(self, obj):
+        return obj.domain.name
+
+    def domain_blocked(self, obj):
+        return obj.domain.blocked
+
+
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(URL, URLAdmin)
